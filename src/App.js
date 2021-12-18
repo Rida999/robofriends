44a4bd2a8925bb2b9eac2a715c0c3f030caf978e
rida ajam <ridaajam999@gmail.com>
2021-12-18 22:24:21 +0200

    making my app more realistic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react'
-import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
@@ -8,9 +7,22 @@ class App extends Component{
     constructor() {
         super();
         this.state={
-            robots:robots,
+            robots:[],
             SearchBox:"",
         }
+    }
+
+    componentDidMount(){
+            fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res=>{
+            return res.json();
+        })
+        .then(users=>{
+            setTimeout(() => {
+            this.setState({robots:users})
+        }, 2000)
+        });
+        
     }
     
     onSearchChange=(e)=>{
@@ -21,13 +33,28 @@ class App extends Component{
         const filteredRobots=this.state.robots.filter((robot)=>{
             return robot.name.toLowerCase().includes(this.state.SearchBox.toLowerCase());
         });
-    return (
-        <div className='tc'>
-            <h1 className='f1'>RoboFriends</h1>
-            <SearchBox onSearchChange={this.onSearchChange} /> {/*this mean app because app is an object*/}
-            <CardList robots={filteredRobots}/>
-        </div>
-    )
-}
+        if(this.state.robots.length===0){
+            return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox onSearchChange={this.onSearchChange} /> {/*this mean app because app is an object*/}
+                <div className='bt bw1'>
+                    <h1>Loading...</h1>
+                </div>
+            </div>
+            )
+        }
+        else{
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox onSearchChange={this.onSearchChange} /> {/*this mean app because app is an object*/}
+                    <div className='bt bw1'>
+                        <CardList robots={filteredRobots}/>
+                    </div>
+                </div>
+            )
+            }
+        }
 }
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Card({name,email,id}) {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}`} alt="later itll be full" style={{width:"200px",height:"200px"}}/>
+            <img src={`https://robohash.org/${id}`} alt="later itll be full" style={{width:"180px",height:"180px"}}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
